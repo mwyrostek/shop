e7337703cb81cb752fec5cf0433942ec1da3bdce
misiu <misiu@misiu-R509.(none)>
2013-03-17 22:16:55 +0100

    first shop include

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = '6443a9d4f45cee36465102b11c4b08c675a18b39c70f55addf22aad57b3c8e18eeec88c8da755286d98e20de0355cf82378eb49c994df8c49d3243646e0c10ba'
